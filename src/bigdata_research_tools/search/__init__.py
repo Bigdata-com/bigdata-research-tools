e9dcb1691628fa4097799f57d6982a9b985383f1
Jelena Starovic <jstarovic@ravenpack.com>
2025-03-02 14:36:14 +0100

    Update init file

--- src/bigdata_research_tools/search/__init__.py
+++ src/bigdata_research_tools/search/__init__.py
@@ -3,5 +3,11 @@ from bigdata_research_tools.search.search import (
     SEARCH_QUERY_RESULTS_TYPE,
     run_search,
 )
+from bigdata_research_tools.search.narrative_search import search_narratives
 
-__all__ = ["SearchManager", "SEARCH_QUERY_RESULTS_TYPE", "run_search"]
+__all__ = [
+    "SearchManager", 
+    "SEARCH_QUERY_RESULTS_TYPE", 
+    "run_search",
+    "search_narratives"
+]
